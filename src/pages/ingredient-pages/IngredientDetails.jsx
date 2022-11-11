20420590a732bb7972a7e1c414f60973ad8fbf48
Helena Saborit <helena.sb91@gmail.com>
2022-11-11 19:36:29 +0100

    created ingredient-pages

---
+++ src/pages/ingredient-pages/IngredientDetails.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function IngredientDetails() {
+
+        //! FER COMPONENT TIPO LAB WIKI BANDERAS?
+        
+
+
+  return (
+    <div>IngredientDetails</div>
+  )
+}
+
+export default IngredientDetails
