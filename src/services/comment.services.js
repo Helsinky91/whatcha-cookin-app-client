6ffa34be6a7603941df624bb1f8670eb71f64b21
Yago <yagoponcem@gmail.com>
2022-11-16 16:35:40 +0100

    implement comments in Recipe Details page

--- src/services/comment.services.js
+++ src/services/comment.services.js
@@ -2,8 +2,8 @@ import service from "./config.services";
 
 
 // create a new comment
-const createCommentService = (recipeId) => {
-    return service.post(`/comment/${recipeId}/create`)
+const createCommentService = (recipeId, comment) => {
+    return service.post(`/comment/${recipeId}/create`, comment)
 }
 
 //get the list of comments of one recipe
@@ -11,9 +11,14 @@ const getCommentService = (recipeId) => {
     return service.get(`/comment/${recipeId}/comment-list`)
 }
 
+//delete a comment
+const deleteCommentService = (commentId) => {
+    return service.delete(`/comment/${commentId}/delete`)
+}
 
 
 export {
     createCommentService,
-    getCommentService
+    getCommentService,
+    deleteCommentService
 }
