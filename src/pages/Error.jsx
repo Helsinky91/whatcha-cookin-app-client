e99f93b001f4e7cbc2e7560bfa51845bd4faa7b0
Helena Saborit <helena.sb91@gmail.com>
2022-11-17 22:24:24 +0100

    styles on error page done

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -3,9 +3,10 @@ import React from 'react'
 function Error() {
   return (
     <div className="error-page">
-       <div>Nuestros desarrolladores cometieron un error, estamos trabjando en ello!</div>
+       {/* <div>Nuestros desarrolladores cometieron un error, estamos trabjando en ello!</div> */}
        {/* <img src="https://thumbs.gfycat.com/AcclaimedChubbyHochstettersfrog-mobile.mp4" /> */}
     
+      <h1 >Nuestros desarrolladores cometieron un error, estamos trabjando en ello!</h1>
     </div>
   )
 }
